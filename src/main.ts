60534455a7b68d305df9dcfbd6603785be35b845
Ibrahim Abdelazim <ibrahimabdelazim@yahoo.com>
2025-09-02 06:47:48 +0300

    refactor: Update Swagger API documentation setup and description in main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,14 +24,14 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Task API')
-    .setDescription('Simple Auth API using NestJS + Mongo + JWT')
+    .setDescription('Task API using NestJS + Mongo')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
