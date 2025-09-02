b4d892f19db17708fc76f16f9c5c06ea9e83dc49
Ibrahim Abdelazim <ibrahimabdelazim@yahoo.com>
2025-09-02 06:46:35 +0300

    feat: Enhance main.ts with security and documentation features, including helmet, rate limiting, CORS support, and Swagger API documentation setup.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+      message: 'Too many requests, please try again later.',
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Task API')
+    .setDescription('Simple Auth API using NestJS + Mongo + JWT')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
